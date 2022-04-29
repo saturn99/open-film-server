79fb012a3b25ae698b506c8c9f95e9d456b982a1
ali(OS10) <email.fivemin@gmail.com>
2022-04-30 02:24:38 +0430

    add --no-check-certificate

--- update.sh
+++ update.sh
@@ -15,6 +15,6 @@ echo $s
 
 dirname=$(echo $s | sed -e 's_https\?://__g')
 
-wget -e robots=off -A "*.html" -r -l 0 -p --convert-links --no-parent "$s"
+wget --no-check-certificate -e robots=off -A "*.html" -r -l 0 -p --convert-links --no-parent "$s"
 
 sed -i -e "s_<!--REPLACE POSITION-->_\t<br>\n\t<a href=\"$dirname\">$dirname</a>\n<!--REPLACE POSITION-->_g" index.html
