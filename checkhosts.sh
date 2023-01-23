cefc4e72a38e9fac0bf4cb7e579cb4dea67b7e61
ali(OS10) <email.fivemin@gmail.com>
2023-01-24 02:50:39 +0330

    update checkhost script [add sort]

--- checkhosts.sh
+++ checkhosts.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cat list | grep -P "^https:/" | httpx -ports 443 -nc -silent -sc -location -title -ip | tee    list.info.txt
-cat list | grep -P "^http:/"  | httpx -ports 80  -nc -silent -sc -location -title -ip | tee -a list.info.txt
+cat list | grep -P "^https:/" | httpx -ports 443 -nc -silent -sc -location -title -ip | tee    /tmp/list.info.txt
+cat list | grep -P "^http:/"  | httpx -ports 80  -nc -silent -sc -location -title -ip | tee -a /tmp/list.info.txt
+
+rm list.info.txt
+sort /tmp/list.info.txt > list.info.txt
+
