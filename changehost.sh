33cfd2e39886387cb94943d5348593394fd747a0
ali(OS10) <email.fivemin@gmail.com>
2023-01-31 01:08:01 +0330

    #39 add changehost

---
+++ changehost.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SEARCH_PATH=${1:?"Please Set SEARCH_PATH"}
+
+ORIGIN_STR=${2:?"Please set string for must be replace"}
+REPLACE_NAME=${2:?"Please set replace name"}
+
+
+find "$SEARCH_PATH" -type f | \
+while read file
+do
+	sed -i -e "s_"$SEARCH_PATH"_"$REPLACE_NAME"_g" "$file"
+done
+
+echo "**************************"
+echo "**************************"
+echo "**************************"
+echo "** 1. change folder's   **"
+echo "** 2. change index.html **"
+echo "**************************"
+echo "**************************"
+echo "**************************"
+
+
