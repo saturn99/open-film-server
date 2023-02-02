59aa29b6da75afd13525e08fa8c797057c9a6d6c
ali(OS10) <email.fivemin@gmail.com>
2023-02-02 19:12:04 +0330

    add -probe to httpx in chechhosts.sh

--- checkhosts.sh
+++ checkhosts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cat list | grep -P "^https:/" | httpx -ports 443 -nc -silent -sc -location -title -ip | tee    /tmp/list.info.txt
-cat list | grep -P "^http:/"  | httpx -ports 80  -nc -silent -sc -location -title -ip | tee -a /tmp/list.info.txt
+cat list | grep -P "^https:/" | httpx -ports 443 -probe -nc -silent -sc -location -title -ip | tee    /tmp/list.info.txt
+cat list | grep -P "^http:/"  | httpx -ports 80  -probe -nc -silent -sc -location -title -ip | tee -a /tmp/list.info.txt
 
 rm list.info.txt
 sort /tmp/list.info.txt > list.info.txt
