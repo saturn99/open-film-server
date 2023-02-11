970c49dbd4ba0beefdcc3e55fcad32b5be3f8f2b
ali(OS10) <email.fivemin@gmail.com>
2023-02-11 23:13:14 +0330

    update update.sh file for save links in etc folder

--- update.sh
+++ update.sh
@@ -11,12 +11,22 @@ echo "----------------"
 read -p "choose for updat: " choosenumber
 read -p "update index.html [y,N]: " updateindex
 
-s=${servers[choosenumber]}
+folder_root="."
+
+if echo ${servers[choosenumber]} | grep -q ",\*"
+then
+	updateindex="n" #set default to no
+	etc_folder=$(echo ${servers[choosenumber]} | cut -d "," -f 2 | tr -d "*")
+	folder_root="etc/${etc_folder}"
+	mkdir -p $folder_root
+fi
+
+s=$(echo ${servers[choosenumber]} | cut -d "," -f 1)
 echo $s
 
 dirname=$(echo $s | sed -e 's_https\?://__g')
 
-/bin/bash wgetcon.sh $s
+/bin/bash wgetcon.sh $s -P $folder_root
 
 if [ ${updateindex:-"n"} == "y" ]
 then
