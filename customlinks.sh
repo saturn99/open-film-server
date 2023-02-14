c2fbbf539ca242255fe01eb505095c25d4558b90
ali(OS10) <email.fivemin@gmail.com>
2023-02-15 02:13:46 +0330

    #20 update custom links for updating

--- customlinks.sh
+++ customlinks.sh
@@ -10,16 +10,32 @@ dirname=${1:?"set dirname"}
 bash wgetcon.sh -i $dirname/$filename
 
 find $dirname  -type d  | 
-   rush -d $'\n' '[ -f "{}/index.html" ] || echo "{}"' |
+   rush -d $'\n' '( [ -f "{}/index.html" ] && [ ! -f "{}/index.u.html" ] ) || echo "{}"' |
    while read d 
    do
    		ls -1 "$d" | 
    		   while read files
    		   do
    		   	    esc_files=$(echo $files | sed -e "s_'_\&#039;_g")
-   		   		echo  "<a href='$esc_files'>$esc_files</a> </br>"
-   		   done > "$d/index.html"
+   		   		echo  "<a href='$esc_files'>$esc_files</a> </br>" >> "$d/index.html"
+                  date  > "$d/index.u.html"
+   		   done 
    done
 
 echo "ok"
 echo "please manual change root index.html"
+
+
+#-----------------------------------
+#      A    |      B      |  X  |
+# -f index  | -f index.u  | echo|
+#-----------------------------------
+#    0      |     0       |   0 |
+#    0      |     1       |   0 |
+#    1      |     0       |   1 | <- echo not be run
+#    1      |     1       |   0 |
+#-----------------------------------
+#      _
+#  A . B = X
+#  A and not(B) = X
+#
